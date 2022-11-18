14f6be1854f9927b3bf7ad31023a3fb92bbb8b41
nia <niaramerquides@gmail.com>
2022-11-18 18:04:52 -0400

    realiza mudanças ate a etapa "operadores 1"

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        // classe string representa texto na aplicaçao
+
+        String meuNome = "Niara Merquides";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    
+        double salarioMinimo =2500;
+      
+        int numero = 1;
+        
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
     }
 }
